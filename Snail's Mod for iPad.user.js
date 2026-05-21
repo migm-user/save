cda3b119260d960b9987f533f43133a9d28b2c21
migm-user <mi08.gm@gmail.com>
2026-05-22 00:40:44 +0900

    Add files via upload

---
+++ Snail's Mod for iPad.user.js
@@ -0,0 +1,83 @@
+// ==UserScript==
+// @name         Snail's Mod for iPad
+// @namespace    O_"
+// @version      1.2.0
+// @match        https://1227719606223765687.discordsays.com/*
+// @match        https://magiccircle.gg/r/*
+// @match        https://magicgarden.gg/r/*
+// @match        https://starweaver.org/r/*
+// @match        https://ariesmod-api.ariedam.fr/*
+// @run-at       document-idle
+// @grant        GM_xmlhttpRequest
+// @grant        GM_info
+// @grant        GM_openInTab
+// @grant        GM_getValue
+// @grant        GM_setValue
+// @grant        GM_registerMenuCommand
+// @grant        GM_download
+// @connect      raw.githubusercontent.com
+// @connect      api.github.com
+// @connect      ariesmod-api.ariedam.fr
+// @connect      mg-api.ariedam.fr
+// @connect      ariedam.fr
+// @connect      cdn.pixabay.com
+// @connect      cdn.jsdelivr.net
+// @connect      magicgarden.gg
+// @connect      i.imgur.com
+// @connect      cdn.discordapp.com
+// @description  o.o_b
+// @downloadURL  https://raw.githubusercontent.com/migm-user/save/main/Snail's%20Mod%20for%20iPad.user.js
+// @updateURL    https://raw.githubusercontent.com/migm-user/save/main/Snail's%20Mod%20for%20iPad.user.js
+// ==/UserScript==
+
+
+//★ 구매
+const autoClickAllButtons = () => {
+
+    // Notifications 버튼 찾기
+    const notificationBtn = document.querySelector('button[aria-label="Notifications"]');
+
+    // 있으면 먼저 클릭
+    if (notificationBtn) {
+        notificationBtn.click();
+        console.log('🔔 Notifications 열기');
+
+    setTimeout(() => {
+        notificationBtn.click();
+        console.log('🔔 Notifications 닫기');
+    }, 500);
+    }
+
+    // 잠깐 기다렸다가 Buy all 클릭
+    setTimeout(() => {
+
+        const allButtons = Array.from(document.querySelectorAll('button'))
+            .filter(btn => btn.textContent.trim() === 'Buy all');
+
+        allButtons.forEach(btn => {
+            if (btn && !btn.disabled) {
+                btn.click();
+                console.log('✅ Buy all 버튼 클릭');
+            }
+        });
+
+    }, 1000); // 1초 기다림
+};
+
+// 10초 뒤 시작 + 2분마다 반복
+setTimeout(autoClickAllButtons, 20000);
+setInterval(autoClickAllButtons, 1 * 60 * 1000);
+//요기가 끝
+
+//Alt X 들어올때마다 한번씩 눌러주기
+setTimeout(() => {
+    ['keydown', 'keyup'].forEach(type => {
+        document.dispatchEvent(new KeyboardEvent(type, {
+            key: 'x',
+            code: 'KeyX',
+            altKey: true,
+            bubbles: true
+        }));
+    });
+}, 15000);
+//요기가 끝
