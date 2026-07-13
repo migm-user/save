1cf10127f189c4c5baf3a91b19d27f7037d983a9
migm-user <mi08.gm@gmail.com>
2026-07-14 02:35:55 +0900

    Update Snail's Mod.user.js

--- Snail's Mod.user.js
+++ Snail's Mod.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Snail's Mod
 // @namespace    O_"
-// @version      1.4.0
+// @version      1.4.1
 // @match        https://1227719606223765687.discordsays.com/*
 // @match        https://magiccircle.gg/r/*
 // @match        https://magicgarden.gg/r/*
